011c6fc6c9d0e9baa409861be093c50d096ef428
karthick <karthylock@gmail.com>
2022-09-20 18:25:07 +0530

    add an greet program

---
+++ Assignments/java-Basics/greet.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class greet {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String name = input.nextLine();
+        System.out.println("Hi " +name +" Have a nice day Buddy!");
+    }
+}
